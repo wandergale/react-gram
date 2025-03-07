67077c6854aed2abd979009771f4dfa826df4919
Wanderson notebook <wandersonlife7@gmail.com>
2025-03-07 18:22:04 -0300

    adding routes on server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const routes = require("./routes/Router");
 
 const PORT = process.env.PORT;
 
@@ -10,6 +11,8 @@ const server = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
+
+server.use(routes);
 
 server.listen(PORT, () => {
   console.log(`App on port: ${PORT}`);
