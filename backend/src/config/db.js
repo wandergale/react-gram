4a9c46eee7a09b3f4b7a3183a371700d948b47ff
Wanderson notebook <wandersonlife7@gmail.com>
2025-03-07 21:33:43 -0300

    getting dbConnection working

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+  try {
+    const dbConn = mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@clusterreactgram.50lxm.mongodb.net/?retryWrites=true&w=majority&appName=ClusterReactGram`
+    );
+
+    console.log("Connected");
+
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+conn();
