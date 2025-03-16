89942165a80ea89f1735f337480c94c71b0884ba
Wanderson notebook <wandersonlife7@gmail.com>
2025-03-15 21:39:54 -0300

    creating first validation for name

---
+++ backend/src/middlewares/userValidations.js
@@ -0,0 +1,7 @@
+const { body } = require("express-validator");
+
+const userCreateValidation = () => {
+  return [body("name").isString().withMessage("O nome é obrigatório")];
+};
+
+module.exports = { userCreateValidation };
