46ce3eb22f28a16bd039a2c53dc1901c57351a01
Wanderson notebook <wandersonlife7@gmail.com>
2025-03-15 22:07:03 -0300

    password & confirmPassword validation

--- backend/src/middlewares/userValidations.js
+++ backend/src/middlewares/userValidations.js
@@ -12,7 +12,20 @@ const userCreateValidation = () => {
       .withMessage("E-mail é obrigatório")
       .isEmail()
       .withMessage("Insira um e-mail válido"),
-    
+    body("password")
+      .isString()
+      .withMessage("A senha é obrigatória")
+      .isLength({ min: 8 })
+      .withMessage("A senha precisa ter 8 caracteres"),
+    body("confirmPassword")
+      .isString()
+      .withMessage("A confirmação da senha é obrigatória")
+      .custom((value, { req }) => {
+        if (value != req.body.password) {
+          throw new Error("Senhas precisam ser iguais");
+        }
+        return true;
+      }),
   ];
 };
 
