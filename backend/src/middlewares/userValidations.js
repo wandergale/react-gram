820872fb2b484ffaae7c0916a25642e0bced6fae
Wanderson notebook <wandersonlife7@gmail.com>
2025-03-15 21:45:24 -0300

    addding new validation to name length >= 3

--- backend/src/middlewares/userValidations.js
+++ backend/src/middlewares/userValidations.js
@@ -1,7 +1,13 @@
 const { body } = require("express-validator");
 
 const userCreateValidation = () => {
-  return [body("name").isString().withMessage("O nome é obrigatório")];
+  return [
+    body("name")
+      .isString()
+      .withMessage("O nome é obrigatório")
+      .isLength({ min: 3 })
+      .withMessage("O nome precisa ter no minimo 3 caracteres."),
+  ];
 };
 
 module.exports = { userCreateValidation };
