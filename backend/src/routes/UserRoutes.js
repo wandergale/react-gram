7e113a82f596e8eb877697411feb66b8764cd49e
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-10 22:59:35 -0300

    middleware

--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 // controller
-const { register } = require("../controllers/UserController");
+const { register, login } = require("../controllers/UserController");
 
 // middlewares
 const validate = require("../middlewares/handleValidation");
-const { userCreateValidation } = require("../middlewares/userValidations");
+const { userCreateValidation, loginValidation } = require("../middlewares/userValidations");
 
 // Route
 router.post("/register", userCreateValidation(), validate, register);
+router.post('/login', loginValidation(), validate, login);
 
 module.exports = router;
