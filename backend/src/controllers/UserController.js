08455733fa4b00c1ceb630c049f5e76b8c191a35
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 16:07:43 -0300

    update user and a new function to hash password

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const mongoose = require("mongoose");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -9,6 +10,14 @@ const generateToken = (id) => {
   return jwt.sign({ id }, jwtSecrete, {
     expiresIn: "7d",
   });
+};
+
+const hashPassword = async (password) => {
+  //Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  return passwordHash;
 };
 
 const register = async (req, res) => {
@@ -22,15 +31,11 @@ const register = async (req, res) => {
     return;
   }
 
-  //Generate password hash
-  const salt = await bcrypt.genSalt();
-  const passwordHash = await bcrypt.hash(password, salt);
-
   //create new user
   const newUser = await User.create({
     name,
     email,
-    password: passwordHash,
+    password: hashPassword(password),
   });
 
   // check if user was created succesfully
@@ -79,7 +84,39 @@ const getCurrentUser = async (req, res) => {
 
 // update an user
 const update = async (req, res) => {
-  res.send("update");
+  const { name, password, bio } = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(
+    new mongoose.Types.ObjectId(reqUser._id)
+  ).select("-password");
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    user.password = hashPassword(password);
+  }
+
+  if (profileImage) {
+    user.profileImage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
 };
 
 module.exports = {
