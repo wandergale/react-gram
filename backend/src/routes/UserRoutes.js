c07b8ebc6f590b126c17da773eacb0112766d6ba
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 15:34:43 -0300

    update route

--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -2,16 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 // controller
-const { register, login, getCurrentUser } = require("../controllers/UserController");
+const {
+  register,
+  login,
+  getCurrentUser,
+  update,
+} = require("../controllers/UserController");
 
 // middlewares
 const validate = require("../middlewares/handleValidation");
-const { userCreateValidation, loginValidation } = require("../middlewares/userValidations");
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/userValidations");
 const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUploads");
 
 // Route
 router.post("/register", userCreateValidation(), validate, register);
-router.post('/login', loginValidation(), validate, login);
-router.get("/profile", authGuard, getCurrentUser)
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 
 module.exports = router;
