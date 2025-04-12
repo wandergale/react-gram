e69bc99ba4c4ba4554744b88beded3e481561eaa
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 12:54:27 -0300

    create user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -12,11 +12,62 @@ const generateToken = (id) => {
 };
 
 const register = async (req, res) => {
-  res.send("Resgistrado");
+  const { name, email, password } = req.body;
+
+  //check if user already exits
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["E-mail ja está cadastrado"] });
+    return;
+  }
+
+  //Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //create new user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // check if user was created succesfully
+  if (!newUser) {
+    res.status(422).json({ errors: ["Erro ao criar usuário"] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
-const login = (req, res) => {
-  res.send("login");
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  //check if user dont exists
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado"] });
+    return;
+  }
+
+  //check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida"] });
+    return;
+  }
+
+  //return user as token
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
 };
 
 module.exports = {
