7c2ab47214570ca000efc72f7653f90138777b39
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 14:58:13 -0300

    image upload & storage

---
+++ backend/src/middlewares/imageUploads.js
@@ -0,0 +1,34 @@
+const multer = require("multer");
+const path = require("path");
+
+// Detination to store image
+const imageStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let folder = "";
+
+    if (req.baseUrl.includes("users")) {
+      folder = "users";
+    } else if (req.baseUrl.includes("photos")) {
+      folder = "photos";
+    }
+
+    cb(null, `uploads/${folder}/`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      // uplaod only png and jpg formats
+      return cb(new Error("Apenas formatos PNG e JPG"));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+module.exports = { imageUpload };
