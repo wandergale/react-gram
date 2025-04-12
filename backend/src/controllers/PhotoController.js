a274dfc228815c984d031d71e3cb9aaf24b42a13
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 17:50:53 -0300

    delete photo controller logic

--- backend/src/controllers/PhotoController.js
+++ backend/src/controllers/PhotoController.js
@@ -29,4 +29,36 @@ const insertPhoto = async (req, res) => {
   res.status(201).json(newPhoto);
 };
 
-module.exports = { insertPhoto };
+// remove phto from db
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+  const reqUser = req.user;
+
+  try {
+    const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+
+    // check if photo exists
+    if (!photo) {
+      res.status(404).json({ errors: ["Foto não encontrada"] });
+      return;
+    }
+
+    // check if photo belongs to user
+    if (!photo.userId.equals(reqUser._id)) {
+      res
+        .status(422)
+        .json({ errors: ["Houve um erro tente novamente mais tarde"] });
+      return;
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    res
+      .status(200)
+      .json({ id: photo._id, message: "Foto excluida com sucesso." });
+  } catch (error) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+  }
+};
+
+module.exports = { insertPhoto, deletePhoto };
