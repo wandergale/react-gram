fc442d5a14efcb8798fdf7d8a2c7c51868976e5f
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 17:26:03 -0300

    photos route

--- backend/src/routes/Router.js
+++ backend/src/routes/Router.js
@@ -3,6 +3,7 @@ const router = express();
 
 // Routes
 router.use("/api/users", require("./UserRoutes"));
+router.use("/api/photos", require("./PhotoRoutes"));
 
 router.get("/", (req, res) => {
   res.send("api working");
