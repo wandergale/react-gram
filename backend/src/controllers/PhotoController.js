4fca023da92ac6bf301a9e926b23d58b9c40dc36
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 17:37:53 -0300

    insert photo logic

--- backend/src/controllers/PhotoController.js
+++ backend/src/controllers/PhotoController.js
@@ -1,14 +1,32 @@
-const photo = require("../models/Photo");
+const Photo = require("../models/Photo");
+const User = require("../models/User");
 
 const mongoose = require("mongoose");
 
 // insert a photo. with an user related to it
-const insertPhoto = (req, res) => {
+const insertPhoto = async (req, res) => {
   const { title } = req.body;
   const image = req.file.filename;
 
-  console.log(req.body);
-  res.send("photo insert");
+  const reqUser = req.user;
+
+  const user = await User.findById(reqUser.id);
+
+  // create photo
+  const newPhoto = await Photo.create({
+    image,
+    title,
+    userId: user._id,
+    userName: user.name,
+  });
+
+  // if photo create with success, return data
+  if (!newPhoto) {
+    res.status(422).json({ errrors: ["Houve um problema"] });
+    return;
+  }
+
+  res.status(201).json(newPhoto);
 };
 
 module.exports = { insertPhoto };
