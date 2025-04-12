84c3cf8f647199646145276f7986e7468d9ee207
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 13:16:43 -0300

    testing authGuard middeware on profile route

--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // controller
-const { register, login } = require("../controllers/UserController");
+const { register, login, getCurrentUser } = require("../controllers/UserController");
 
 // middlewares
 const validate = require("../middlewares/handleValidation");
 const { userCreateValidation, loginValidation } = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
 
 // Route
 router.post("/register", userCreateValidation(), validate, register);
 router.post('/login', loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser)
 
 module.exports = router;
