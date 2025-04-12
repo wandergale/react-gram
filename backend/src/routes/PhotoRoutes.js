63627a2afeeef7a1967b240f741b13d1d0680242
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 17:25:52 -0300

    insert photo route

--- backend/src/routes/PhotoRoutes.js
+++ backend/src/routes/PhotoRoutes.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
+const { insertPhoto } = require("../controllers/PhotoController");
 
 // Middlewares
 const { photoInsertValidation } = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
-// Routes
+const { imageUpload } = require("../middlewares/imageUploads");
 
-module.exports = { router };
+// Routes
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+);
+
+module.exports = router;
