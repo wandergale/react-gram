2191db1dab5458df97bc1190078cb9a43e92eefa
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 22:51:33 -0300

    search photo route

--- backend/src/routes/PhotoRoutes.js
+++ backend/src/routes/PhotoRoutes.js
@@ -11,6 +11,7 @@ const {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 } = require("../controllers/PhotoController");
 
 // Middlewares
@@ -34,6 +35,7 @@ router.post(
 );
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
+router.get("/search", authGuard, searchPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
