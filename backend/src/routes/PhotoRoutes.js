7fc810fa9576a39685344ba5e2a9a88da1680c64
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 22:44:59 -0300

    adding comments on photo

--- backend/src/routes/PhotoRoutes.js
+++ backend/src/routes/PhotoRoutes.js
@@ -10,12 +10,14 @@ const {
   getPhotoById,
   updatePhoto,
   likePhoto,
+  commentPhoto,
 } = require("../controllers/PhotoController");
 
 // Middlewares
 const {
   photoInsertValidation,
   photoUpdateValidation,
+  commentValidation,
 } = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
@@ -36,5 +38,12 @@ router.get("/user/:id", authGuard, getUserPhotos);
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 router.put("/like/:id", authGuard, likePhoto);
+router.put(
+  "/comments/:id",
+  authGuard,
+  commentValidation(),
+  validate,
+  commentPhoto
+);
 
 module.exports = router;
