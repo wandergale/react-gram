b4e23d0bc83c433cfef8f7cc20d94ef13c096a0c
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-12 21:52:35 -0300

    update photo rote & middlewares

--- backend/src/routes/PhotoRoutes.js
+++ backend/src/routes/PhotoRoutes.js
@@ -8,10 +8,14 @@ const {
   getAllPhotos,
   getUserPhotos,
   getPhotoById,
+  updatePhoto,
 } = require("../controllers/PhotoController");
 
 // Middlewares
-const { photoInsertValidation } = require("../middlewares/photoValidation");
+const {
+  photoInsertValidation,
+  photoUpdateValidation,
+} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const { imageUpload } = require("../middlewares/imageUploads");
@@ -29,5 +33,6 @@ router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
 router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 
 module.exports = router;
